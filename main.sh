9acbcee64e912c5611ebbf783a0f9439d1f40557
alectramell <alectramell@gmail.com>
2017-08-18 14:11:32 -0700

    beacons

--- main.sh
+++ main.sh
@@ -2,27 +2,61 @@
 
 clear
 
-echo -n "beacon: "
-read TAG
+TAG=$((1 + RANDOM % 3))
 
 clear
 
-rm $(pwd)/$TAG.bcn
-
-clear
-
-wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/$TAG.bcn -O $(pwd)/$TAG.bcn
-
-clear
-
-PXP=$(wc -c < $(pwd)/$TAG.bcn)
-POP=$PXP
-
-while [ $POP -gt 5 ]
+while [ $TAG -eq 1 ]
 do
-	echo -n "$TAG = "
-	echo "$POP"
-	sleep 1.5
 	clear
+	rm $(pwd)/001.bcn
+	clear
+	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/001.bcn -O $(pwd)/001.bcn
+	clear
+
+	PXP=$(wc -c < $(pwd)/001.bcn)
+	POP=$PXP
+
+	clear
+
+	echo "BEACONS = $POP"
+	sleep 3
+
+done
+
+while [ $TAG -eq 2 ]
+do
+	clear
+	rm $(pwd)/002.bcn
+	clear
+	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/002.bcn -O $(pwd)/002.bcn
+	clear
+
+	PXP=$(wc -c < $(pwd)/002.bcn)
+	POP=$PXP
+
+	clear
+
+	echo "BEACONS = $POP"
+	sleep 3
+
+done
+
+while [ $TAG -eq 3 ]
+do
+	clear
+	rm $(pwd)/003.bcn
+	clear
+	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/003.bcn -O $(pwd)/003.bcn
+	clear
+
+	PXP=$(wc -c < $(pwd)/003.bcn)
+	POP=$PXP
+
+	clear
+
+	echo "BEACONS = $POP"
+	sleep 3
+
 done
 
