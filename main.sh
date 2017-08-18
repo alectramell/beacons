fc644a06a6bc6f04122c7f0de051e3d0ab2b17d8
alectramell <alectramell@gmail.com>
2017-08-18 14:39:05 -0700

    beacons

--- main.sh
+++ main.sh
@@ -4,6 +4,11 @@ clear
 
 TAG=$((1 + RANDOM % 3))
 
+red=$(tput setaf 1)
+yellow=$(tput setaf 190)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+
 clear
 
 while [ $(cat $(pwd)/status.txt) = "ON" ]
@@ -14,49 +19,49 @@ TAG=$((1 + RANDOM % 3))
 if [ $TAG -eq 1 ]
 then
 	clear
-	rm $(pwd)/001.bcn
+	rm $(pwd)/red.bcn
 	clear
-	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/001.bcn -O $(pwd)/001.bcn
+	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/red.bcn -O $(pwd)/red.bcn
 	clear
 
-	PXP=$(wc -c < $(pwd)/001.bcn)
+	PXP=$(wc -c < $(pwd)/red.bcn)
 	POP=$PXP
 
 	clear
 
-	echo "BEACONS = $POP"
+	echo "BEACONS (${red}red${reset}) = $POP"
 	sleep 3
 
 elif [ $TAG -eq 2 ]
 then
 	clear
-	rm $(pwd)/002.bcn
+	rm $(pwd)/yellow.bcn
 	clear
-	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/002.bcn -O $(pwd)/002.bcn
+	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/yellow.bcn -O $(pwd)/yellow.bcn
 	clear
 
-	PXP=$(wc -c < $(pwd)/002.bcn)
+	PXP=$(wc -c < $(pwd)/yellow.bcn)
 	POP=$PXP
 
 	clear
 
-	echo "BEACONS = $POP"
+	echo "BEACONS (${yellow}yellow${reset}) = $POP"
 	sleep 3
 
 elif [ $TAG -eq 3 ]
 then
 	clear
-	rm $(pwd)/003.bcn
+	rm $(pwd)/green.bcn
 	clear
-	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/003.bcn -O $(pwd)/003.bcn
+	wget --quiet https://raw.githubusercontent.com/alectramell/beacons/master/green.bcn -O $(pwd)/green.bcn
 	clear
 
-	PXP=$(wc -c < $(pwd)/003.bcn)
+	PXP=$(wc -c < $(pwd)/green.bcn)
 	POP=$PXP
 
 	clear
 
-	echo "BEACONS = $POP"
+	echo "BEACONS (${green}green${reset}) = $POP"
 	sleep 3
 fi
 
