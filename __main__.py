59478e00413151478e4837662c4728c3aee3b560
alectramell <alectramell@gmail.com>
2017-08-21 21:43:33 -0700

    beacons

--- __main__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-os.system('bash /home/$(whoami)/Desktop/beacons/main.sh')
--- render.sh
+++ render.sh
@@ -2,34 +2,6 @@
 
 clear
 
-echo -n "ACTIVE STATUS (t/f): "
-read -s -n1 STATUS
-
-clear
-
-if [ $STATUS = "t" ]
-then
-	echo "ON" > $(pwd)/status.txt
-	clear
-	echo "ACTIVE STATUS = TRUE" | pv -qL 10
-	sleep 1.0
-
-elif [ $STATUS = "f" ]
-then
-	echo "OFF" > $(pwd)/status.txt
-	clear
-	echo "ACTIVE STATUS = FALSE" | pv -qL 10
-	sleep 1.0
-else
-	echo "ACTIVE STATUS = VOID"
-	sleep 1.0
-	clear
-	exit
-fi
-
-clear
-
-git add $(pwd)/*.bcn
 git add $(pwd)/*.sh
 git add $(pwd)/*.py
 git add $(pwd)/*.txt
