a7623699576c67e90f1d64d3318cccd69377fffe
alectramell <alectramell@gmail.com>
2017-09-05 12:02:59 -0700

    beacons

--- updates.sh
+++ updates.sh
@@ -2,14 +2,10 @@
 
 clear
 
-wget -A="svg" -N -O /home/$(whoami)/admshell.svg https://raw.githubusercontent.com/alectramell/beacons/master/admshell.svg
-
-clear
-
 UPDATES=$(curl -s https://raw.githubusercontent.com/alectramell/beacons/master/updates.txt)
 
 clear
 
-notify-send --urgency="critical" --icon="/home/$(whoami)/admshell.svg" "$UPDATES" "www.facebook.com/apollondma" &
+echo "$UPDATES" | pg
 
 clear
