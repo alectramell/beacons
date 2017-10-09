ac018ed779012237da7b7f30f96548a978dd5610
alectramell <alectramell@gmail.com>
2017-10-09 02:56:42 -0700

    beacons

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 function checkStatus() {
+	document.getElementById('BG').background = 'img/bg.jpg';
 	document.getElementById('status').src = 'https://raw.githubusercontent.com/alectramell/beacons/master/img/online.png';
-	document.getElementById('BG').background = 'https://raw.githubusercontent.com/alectramell/beacons/master/img/bg.jpg';
 }
 
