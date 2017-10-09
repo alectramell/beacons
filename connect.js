4c3edef5db1523f90596ee32ec23c7d44fdd2981
alectramell <alectramell@gmail.com>
2017-10-09 02:49:16 -0700

    beacons

--- connect.js
+++ connect.js
@@ -1,9 +1,5 @@
-getStatus = true;
-
-if (getStatus = true) {
-	function checkStatus() {
-		document.getElementById('status').src = 'https://raw.githubusercontent.com/alectramell/beacons/master/img/online.png';
-		document.getElementById('BG').background = 'img/bg.jpg';
-	}
+function checkStatus() {
+	document.getElementById('status').src = 'https://raw.githubusercontent.com/alectramell/beacons/master/img/online.png';
+	document.getElementById('BG').background = 'https://raw.githubusercontent.com/alectramell/beacons/master/img/bg.jpg';
 }
 
