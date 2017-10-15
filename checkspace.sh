de34d8dac9288ad8e658e21888f8070c3dfc6293
alectramell <alectramell@gmail.com>
2017-10-15 00:50:05 -0700

    beacons

---
+++ checkspace.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+white=$(tput setaf 7)
+bold=$(tput bold)
+reset=$(tput sgr0)
+
+clear
+
+DLIB=($(df -h /dev/sda2 | grep "/dev/sda2"))
+COUNT=$(echo "${DLIB[4]}" | grep -oP '.*?(?=\%)')
+LEFT=$(echo $((100-$COUNT)))
+
+clear
+
+if [ $COUNT -lt 50 ]
+then
+	echo "${bold}${white}You have used ${green}$COUNT${white} percent of your local drive space..${reset}"
+	notify-send --urgency="critical" --icon="/home/$(whoami)/icons/voodoo.svg" "$LEFT%" "FREESPACE"
+	notify-send --urgency="critical" --icon="/home/$(whoami)/icons/voodoo.svg" "${DLIB[4]}" "USED"
+
+elif [ $COUNT -gt 50 ]
+then
+	echo "${bold}${white}You have used ${red}$COUNT${white} percent of your local drive space..${reset}"
+	notify-send --urgency="critical" --icon="/home/$(whoami)/icons/voodoo.svg" "$LEFT%" "FREESPACE"
+	notify-send --urgency="critical" --icon="/home/$(whoami)/icons/voodoo.svg" "${DLIB[4]}" "USED"
+fi
+
+sleep 3
+
+clear
+
+
