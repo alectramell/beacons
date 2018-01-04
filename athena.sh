cb33ed688c24d654df919a2414237c9269f983b9
alectramell <alectramell@gmail.com>
2018-01-04 10:46:50 -0800

    beacons

---
+++ athena.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Usage:
+# Open Terminal and import ATHENA by entering the following command..
+
+# source <(curl -s https://raw.githubusercontent.com/alectramell/beacons/master/athena.sh)
+
+# After importing, to use athena variables, simply enter them into your script..
+
+athenaBOX() {
+
+	echo "xxxxx"
+	echo "xxxxx"
+	echo "xxxxx"
+}
